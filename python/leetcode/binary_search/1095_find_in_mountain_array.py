9e220348971bca86d14ade1b79da1434363c1599
Zhuoyuan Chen <zhuoyuanchen2014@u.northwestern.edu>
2019-06-23 15:13:14 -0700

    contest

---
+++ python/leetcode/binary_search/1095_find_in_mountain_array.py
@@ -0,0 +1,138 @@
+"""
+1095. Find in Mountain Array (Hard)
+
+(This problem is an interactive problem.)
+
+You may recall that an array A is a mountain array if and only if:
+
+A.length >= 3
+There exists some i with 0 < i < A.length - 1 such that:
+A[0] < A[1] < ... A[i-1] < A[i]
+A[i] > A[i+1] > ... > A[A.length - 1]
+Given a mountain array mountainArr, return the minimum index such that mountainArr.get(index) == target.  If such an index doesn't exist, return -1.
+
+You can't access the mountain array directly.  You may only access the array using a MountainArray interface:
+
+MountainArray.get(k) returns the element of the array at index k (0-indexed).
+MountainArray.length() returns the length of the array.
+Submissions making more than 100 calls to MountainArray.get will be judged Wrong Answer.  Also, any solutions that attempt to circumvent the judge will result in disqualification.
+
+Example 1:
+
+Input: array = [1,2,3,4,5,3,1], target = 3
+Output: 2
+Explanation: 3 exists in the array, at index=2 and index=5. Return the minimum index, which is 2.
+Example 2:
+
+Input: array = [0,1,2,4,2,1], target = 3
+Output: -1
+Explanation: 3 does not exist in the array, so we return -1.
+
+Constraints:
+
+3 <= mountain_arr.length() <= 10000
+0 <= target <= 10^9
+0 <= mountain_arr.get(index) <= 10^9
+"""
+
+# """
+# This is MountainArray's API interface.
+# You should not implement it, or speculate about its implementation
+# """
+#class MountainArray(object):
+#    def get(self, index):
+#        """
+#        :type index: int
+#        :rtype int
+#        """
+#
+#    def length(self):
+#        """
+#        :rtype int
+#        """
+
+class MountainArray(object):
+    def __init__(self, arr):
+        self.arr = arr
+
+    def get(self, index):
+        """
+        :type index: int
+        :rtype int
+        """
+        if index < 0 or index >= len(self.arr):
+            raise RuntimeError
+        return self.arr[index]
+
+    def length(self):
+        """
+        :rtype int
+        """
+        return len(self.arr)
+
+
+class Solution(object):
+    def findInMountainArray(self, target, mountain_arr):
+        """
+        :type target: integer
+        :type mountain_arr: MountainArray
+        :rtype: integer
+        """
+        # step 1: find the peak
+        n = mountain_arr.length()
+        i, j = 0, n-1
+        peak = None
+        while i <= j:
+            mid = (i + j) // 2
+            # print(mid)
+            num = mountain_arr.get(mid)
+            if mid > 0:
+                left = mountain_arr.get(mid-1)
+            else:
+            	left = None
+            if mid != n-1:
+                right = mountain_arr.get(mid+1)
+            else:
+            	right = None
+            if left is not None and num > left and right is not None and num > right:
+                peak = mid
+                break
+            elif left is None or (num > left and right > num):
+                i = mid + 1
+            else:
+            	j = mid - 1
+        # step 2: search in [0..peak]
+        i, j = 0, peak
+        if target == mountain_arr.get(peak): return peak
+        while i <= j:
+            mid = (i + j) // 2
+            num = mountain_arr.get(mid)
+            if num == target:
+                return mid
+            elif num > target:
+            	j = mid - 1
+            else:
+            	i = mid + 1
+        # step 3: search in [peak..end-1]
+        i, j = peak, n-1
+        if target == mountain_arr.get(peak): return peak
+        while i <= j:
+            mid = (i + j) // 2
+            num = mountain_arr.get(mid)
+            if num == target:
+                return mid
+            elif num < target:
+            	j = mid - 1
+            else:
+            	i = mid + 1
+        return -1
+
+
+if __name__ == "__main__":
+    a = Solution()
+    # mountain_arr = MountainArray([1,2,3,4,5,3,1])
+    # mountain_arr = MountainArray([3,5,3,2,0])
+    mountain_arr = MountainArray([0, 2, 3, 5, 3])
+    print(a.findInMountainArray(3, mountain_arr))
+    mountain_arr = MountainArray([0,1,2,4,2,1])
+    print(a.findInMountainArray(3, mountain_arr))
---
+++ python/leetcode/combinatorial/1096_brace_expansion.py
@@ -0,0 +1,174 @@
+"""
+1096. Brace Expansion II (Hard)
+
+Under a grammar given below, strings can represent a set of lowercase words.  Let's use R(expr) to denote the set of words the expression represents.
+
+Grammar can best be understood through simple examples:
+
+Single letters represent a singleton set containing that word.
+R("a") = {"a"}
+R("w") = {"w"}
+When we take a comma delimited list of 2 or more expressions, we take the union of possibilities.
+R("{a,b,c}") = {"a","b","c"}
+R("{{a,b},{b,c}}") = {"a","b","c"} (notice the final set only contains each word at most once)
+When we concatenate two expressions, we take the set of possible concatenations between two words where the first word comes from the first expression and the second word comes from the second expression.
+R("{a,b}{c,d}") = {"ac","ad","bc","bd"}
+R("{a{b,c}}{{d,e},f{g,h}}") = R("{ab,ac}{dfg,dfh,efg,efh}") = {"abdfg", "abdfh", "abefg", "abefh", "acdfg", "acdfh", "acefg", "acefh"}
+Formally, the 3 rules for our grammar:
+
+For every lowercase letter x, we have R(x) = {x}
+For expressions e_1, e_2, ... , e_k with k >= 2, we have R({e_1,e_2,...}) = R(e_1) ∪ R(e_2) ∪ ...
+For expressions e_1 and e_2, we have R(e_1 + e_2) = {a + b for (a, b) in R(e_1) × R(e_2)}, where + denotes concatenation, and × denotes the cartesian product.
+Given an expression representing a set of words under the given grammar, return the sorted list of words that the expression represents.
+
+ 
+
+Example 1:
+
+Input: "{a,b}{c{d,e}}"
+Output: ["acd","ace","bcd","bce"]
+Example 2:
+
+Input: "{{a,z},a{b,c},{ab,z}}"
+Output: ["a","ab","ac","z"]
+Explanation: Each distinct word is written only once in the final answer.
+
+Constraints:
+
+1 <= expression.length <= 50
+expression[i] consists of '{', '}', ','or lowercase English letters.
+The given expression represents a set of words based on the grammar given in the description.
+"""
+
+"""
+Solution 1: two functions (split and concatenate calls each other)
+
+Solution 2:
+The general idea
+Split expressions into groups separated by top level ','; for each top-level sub expression (substrings with braces), process it and add it to the corresponding group; finally combine the groups and sort.
+
+Thought process
+in each call of the function, try to remove one level of braces
+maintain a list of groups separated by top level ','
+when meets ',': create a new empty group as the current group
+when meets '{': check whether current level is 0, if so, record the starting index of the sub expression;
+always increase level by 1, no matter whether current level is 0
+when meets '}': decrease level by 1; then check whether current level is 0, if so, recursively call braceExpansionII to get the set of words from expresion[start: end], where end is the current index (exclusive).
+add the expanded word set to the current group
+when meets a letter: check whether the current level is 0, if so, add [letter] to the current group
+base case: when there is no brace in the expression.
+finally, after processing all sub expressions and collect all groups (seperated by ','), we initialize an empty word_set, and then iterate through all groups
+for each group, find the product of the elements inside this group
+compute the union of all groups
+sort and return
+note that itertools.product(*group) returns an iterator of tuples of characters, so we use''.join() to convert them to strings
+"""
+
+class Solution(object):
+    def braceExpansionII(self, expression):
+        """
+        :type expression: str
+        :rtype: List[str]
+        """
+        # first split
+        def split(exp):
+            print(exp)
+            expl = []
+            tmp = ""
+            cnt = 0
+            n = len(exp)
+            for i in range(n):
+                c = exp[i]
+                if c == "{":
+                    tmp += c
+                    cnt += 1
+                elif c == "}": # or i == n-1:
+                    tmp += c
+                    cnt -= 1
+                    if cnt == 0 and (i == n-1 or exp[i+1]!=','):
+                        if tmp[0] == "{":
+                            tmp = tmp[1:-1]
+                        expl.append(tmp)
+                        tmp = ""
+                else:
+                    tmp += c
+                    if i == n-1 or (cnt == 0 and exp[i+1] == "{"):
+                        expl.append(tmp)
+                        tmp = ""
+            return expl
+        # expl = split(expression)
+        # print(expl)
+
+        def split2(exp):
+            # split by ","
+            expl = exp.split(",")
+            n = len(expl)
+            result = []
+            cnt = 0
+            tmp = ""
+            for i in range(n):
+                if tmp != "": tmp += ","
+                tmp += expl[i]
+                cnt = tmp.count("{") - tmp.count("}")
+                if cnt == 0:
+                    # if tmp[0] == "{":
+                    #     tmp = tmp[1:-1]
+                    result.append(tmp)
+                    tmp = ""
+            return result
+        # step 2: for each split, generate most
+        def generate(exp):
+            if "{" not in exp:
+                tmpl = exp.split(",")
+                return tmpl
+            else:
+                expl = split(exp)
+                result = []
+                for subexp in expl:
+                    subres = generate2(subexp)
+                    result.append(subres)
+                full_result = result[0]
+                for i in range(1, len(result)):
+                    full_result = [item1+item2 for item1 in full_result for item2 in result[i]]
+                    full_result = list(set(full_result))
+                return full_result
+        
+        def generate2(exp):        
+            expl = split2(exp)
+            result = set()
+            for item in expl:
+                subres = generate(item)
+                for item in subres:
+                    result.add(item)
+            result = list(result)
+            return result
+        result = generate(expression)
+        result.sort()
+        return result
+
+    def solve2(self, expression: str) -> List[str]:
+        groups = [[]]
+        level = 0
+        for i, c in enumerate(expression):
+            if c == '{':
+                if level == 0:
+                    start = i+1
+                level += 1
+            elif c == '}':
+                level -= 1
+                if level == 0:
+                    groups[-1].append(self.braceExpansionII(expression[start:i]))
+            elif c == ',' and level == 0:
+                groups.append([])
+            elif level == 0:
+                groups[-1].append([c])
+        word_set = set()
+        for group in groups:
+            word_set |= set(map(''.join, itertools.product(*group)))
+        return sorted(word_set)
+
+if __name__ == "__main__":
+    a = Solution()
+    # print(a.braceExpansionII("{a,b}{c{d,e}}"))
+    # print(a.braceExpansionII("{{a,z},a{b,c},{ab,z}}"))
+    print(a.braceExpansionII("{{{c,g},{h,j},l},{a,{x,ia,o},w},{x,ia,o}}"))
